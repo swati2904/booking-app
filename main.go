4992a75587fe16f3297041759d11a8e7e0cba2c3
Swati Saxena <swati.lpu17@gmail.com>
2024-08-11 20:51:53 -0700

    fixed array constant size problem with slice

--- main.go
+++ main.go
@@ -8,7 +8,10 @@ func main() {
 	var remainingTickets uint = 50
 	// var bookings = [50]string{"user1", "user2", "user3"}
 	// alternative syntax
-	var bookings [50]string
+	// var bookings [50]string
+
+	//slice
+	var bookings []string
 
 	// To check the data type of variables
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
@@ -47,13 +50,21 @@ func main() {
 
 	remainingTickets = remainingTickets - userTickets
 
-	bookings[0] = firstName + " " + lastName
+	// bookings[0] = firstName + " " + lastName
+	bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("The whole array: %v\n", bookings)
-	fmt.Printf("The first array: %v\n", bookings[0])
-	fmt.Printf("Array type: %T\n", bookings)
-	fmt.Printf("Array length: %v\n", len(bookings))
+	// fmt.Printf("The whole array: %v\n", bookings)
+	// fmt.Printf("The first array: %v\n", bookings[0])
+	// fmt.Printf("Array type: %T\n", bookings)
+	// fmt.Printf("Array length: %v\n", len(bookings))
+
+	// fmt.Printf("The whole slice: %v\n", bookings)
+	// fmt.Printf("The first slice: %v\n", bookings[0])
+	// fmt.Printf("slice type: %T\n", bookings)
+	// fmt.Printf("slice length: %v\n", len(bookings))
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v or an SMS on your phone Number %v\n", firstName, lastName, userTickets, email, phoneNumber)
-	fmt.Printf("%v tickets remaining for %v", remainingTickets, conferenceName)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+	fmt.Printf("These are all the our booking: %v\n", bookings)
 }
