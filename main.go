c0e14595d8d968862524e31ac44e28c0d3f14ea1
Swati Saxena <swati.lpu17@gmail.com>
2024-08-11 18:25:08 -0700

    explicitly define data types and printing user details

--- main.go
+++ main.go
@@ -3,12 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	const conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	const conferenceName string = "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets int = 50
 
-	fmt.Println("Welcome to", conferenceName, "booking application")
-	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still avaiable.")
+	// To check the data type of variables
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still avaiable.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	// Define data type explicitly
+	var userName string
+	var userTickets int
+
+	// Ask user for their name
+	userName = "Jonathan"
+	userTickets = 2
+	fmt.Printf("user %v booked %v tickets.\n", userName, userTickets)
 
 }
