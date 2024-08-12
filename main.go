1df67f6aededcba2ed95177b9504871c689b6a1c
Swati Saxena <swati.lpu17@gmail.com>
2024-08-11 18:04:52 -0700

    setup and initialized fucntion

--- main.go
+++ main.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello World")
+	const conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still avaiable.")
+	fmt.Println("Get your tickets here to attend")
+
 }
