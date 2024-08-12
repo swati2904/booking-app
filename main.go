adc47c644c0653bc40a0e5a9e54935f3a4da1992
Swati Saxena <swati.lpu17@gmail.com>
2024-08-12 00:21:30 -0700

    implemented scope rules principle

--- main.go
+++ main.go
@@ -67,14 +67,6 @@ func getFirstName() []string {
 	return firstNames
 }
 
-func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
-	isValidName := len(firstName) >= 2 && len(lastName) >= 2
-	isValidEmail := strings.Contains(email, "@")
-	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
-
-	return isValidName, isValidEmail, isValidTicketNumber
-}
-
 func getUserInput() (string, string, string, uint) {
 	// Define data type explicitly
 	var firstName string
